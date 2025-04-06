be43b48c048c2c5970ba771f740e294702c8e946
unknown <contact.sese.a@gmail.com>
2025-04-06 22:58:08 +0300

    This commit was made after the vercel deployment changes were made

--- middleware/loggerMiddleware.js
+++
@@ -1,8 +0,0 @@
-const logger = (req, res, next) => {
-    const path = req.path 
-    const method = req.method
-    console.log(`Incoming ${method} request to ${path}`)
-    next()
-}
-
-module.exports = logger
--- paths/usdPath.js
+++
@@ -1,7 +0,0 @@
-const path = require("path")
-
-const directoryPath = path.join(__dirname, "../rateData")
-const filePath = path.join(directoryPath, "ngnUsd.json")
-const usdPath = filePath
-
-module.exports = {usdPath}
