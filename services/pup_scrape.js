70992ba616c2fe3b7aa2a82122be3b80af3a3817
unknown <contact.sese.a@gmail.com>
2025-04-10 12:41:18 +0300

    This commit was made after I sdded a scheduler to my vercel.json file for the pup-scrape

--- services/pup_scrape.js
+++ services/pup_scrape.js
@@ -4,24 +4,33 @@ const { file_data } = require("./file_access");
 
 module.exports = {
     get_pup_data: async () => {
+        // console.time("puppeteer")
         const timestamp = new Date()
 
         try {
             const browser = await puppeteer.launch({ 
-                    args: chromium.args,
+                    args: [...chromium.args, "--no-sandbox", "--disable-setuid-sandbox"],
                     executablePath: await chromium.executablePath(),
                     headless: chromium.headless,
                 });
 
                 const page = await browser.newPage()
                 await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36')
-                await page.goto('https://usd.currencyrate.today/ngn', { waitUntil: 'domcontentloaded' })
                 await page.goto('https://usd.currencyrate.today/ngn', { waitUntil: 'networkidle2' });
                 await page.waitForSelector('#fex-rates')
+                await page.setRequestInterception(true);
+                page.on("request", (req) => {
+                    if (["image", "stylesheet", "font"].includes(req.resourceType())) {
+                        req.abort()
+                    } else {
+                        req.continue()
+                    }
+                })
                 const cbnBuyRate = await page.$eval('td[headers="ng-cbn header-buy"]', el => el.textContent.trim())
                 const cbnSellRate = await page.$eval('td[headers="ng-cbn header-sell"]', el => el.textContent.trim())
                 const blackMarketBuyRate = await page.$eval('td[headers="ng-nairablackmarket header-buy"]', el => el.textContent.trim())
                 const blackMarketSellRate = await page.$eval('td[headers="ng-nairablackmarket header-sell"]', el => el.textContent.trim())
+                // console.timeEnd("puppeteer")
                 await browser.close()
 
                 let responseData = []
@@ -39,6 +48,7 @@ module.exports = {
                 }
 
                 responseData.push(response)
+                // console.log(responseData)
 
                 await file_data(responseData)
 
