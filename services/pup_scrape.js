7d84ac13a699d351a2a48bd82c84d78b1fc51462
unknown <contact.sese.a@gmail.com>
2025-04-10 12:12:30 +0300

    This commit was made after the initial implementation of the pupeteer scrapping with chromium for the vercel serverless implenetation

--- services/pup_scrape.js
+++ services/pup_scrape.js
@@ -1,4 +1,5 @@
 const puppeteer = require("puppeteer");
+const chromium = require("@sparticuz/chromium")
 const { file_data } = require("./file_access");
 
 module.exports = {
@@ -6,11 +7,16 @@ module.exports = {
         const timestamp = new Date()
 
         try {
-            const browser = await puppeteer.launch({ headless: true });
+            const browser = await puppeteer.launch({ 
+                    args: chromium.args,
+                    executablePath: await chromium.executablePath(),
+                    headless: chromium.headless,
+                });
+
                 const page = await browser.newPage()
                 await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36')
                 await page.goto('https://usd.currencyrate.today/ngn', { waitUntil: 'domcontentloaded' })
-                await page.goto('https://usd.currencyrate.today/ngn', { waitUntil: 'networkidle2' });
+                await page.goto('https://usd.currencyrate.today/ngn', { waitUntil: 'networkidle2', timeout: 10000 });
                 await page.waitForSelector('#fex-rates')
                 const cbnBuyRate = await page.$eval('td[headers="ng-cbn header-buy"]', el => el.textContent.trim())
                 const cbnSellRate = await page.$eval('td[headers="ng-cbn header-sell"]', el => el.textContent.trim())
@@ -40,6 +46,7 @@ module.exports = {
         } catch (err) {
             console.log(err)
             console.log(err.message)
+            throw new Error("Failed to fetch currency rates due to Puppeteer issue")
         }
     }
 }
